b56488eebdd6b37caf919d69e5dcbfeefb70a623
Jayaram Kancherla <jayaram.kancherla@gmail.com>
2016-08-01 12:41:33 -0400

    send http requests to neo4j,  using ujson for json encodign

--- jay_get_combined.py
+++ jay_get_combined.py
@@ -1,15 +1,16 @@
 from flask import Flask, jsonify, request
-from py2neo import Graph
+# from py2neo import Graph
 import pandas
 import credential
-# import ujson
+import requests as rqs
+import ujson
 
 app = Flask(__name__)
 # app.json_encoder = MiniJSONEncoder
 app.config['JSONIFY_PRETTYPRINT_REGULAR'] = False
 
-graph = Graph(password=credential.neo4j_password)
-cypher = graph
+# graph = Graph(password=credential.neo4j_password)
+# cypher = graph
 
 # Function to handle access control allow headers
 def add_cors_headers(response):
@@ -43,30 +44,46 @@ def post_combined():
 
     tick_samples = in_samples.replace("\"", "\'")
 
-    # get the min selected Level if aggregated at multiple levels 
+    # get the min selected Level if aggregated at multiple levels
     minSelectedLevel = 100
     for level in in_params_selectedLevels.keys():
         if in_params_selectedLevels[level] == 2 and int(level) < minSelectedLevel:
             minSelectedLevel = int(level)
 
-    # user selection nodes for custom aggregation - decides the cut 
+    # user selection nodes for custom aggregation - decides the cut
     selNodes = "["
     selFlag = 0
     for node in in_params_selection.keys():
         if in_params_selection[node] == 2:
-            selNodes += "'" + node + "'"
+            selNodes += "'" +  node + "',"
             selFlag = 1
 
-    if selFlag ==1:
+    if selFlag == 1:
         selNodes = selNodes[:-1]
-
     selNodes += "]"
 
-    qryStr = "MATCH (f:Feature)-[:LEAF_OF]->()<-[v:VALUE]-(s:Sample) WHERE (f.depth='" + str(minSelectedLevel) + "' OR f.id IN " + selNodes + ") AND s.id IN " + tick_samples + " with distinct f, s, SUM(v.val) as agg RETURN distinct agg, s.id, f.label as label, f.leafIndex as index, f.end as end, f.start as start, f.id as id, f.lineage as lineage, f.lineageLabel as lineageLabel, f.order as order"
-    result = cypher.data(qryStr)
+    qryStr = "MATCH (f:Feature)-[:LEAF_OF]->(leaf:Feature)<-[v:VALUE]-(s:Sample) USING INDEX s:Sample(id) WHERE (f.depth='" + str(
+        minSelectedLevel) + "' OR f.id IN " + selNodes + ") AND (leaf.start >= '" + in_params_start + "' AND leaf.end < '" + in_params_end + "') AND s.id IN " + tick_samples + " with distinct f, s, SUM(v.val) as agg RETURN distinct agg, s.id, f.label as label, f.leafIndex as index, f.end as end, f.start as start, f.id as id, f.lineage as lineage, f.lineageLabel as lineageLabel, f.order as order"
+    print(qryStr)
+    # result = cypher.data(qryStr)
 
+    headers = {'Content-Type': 'application/json'}
+    data = {'statements': [{'statement': qryStr, 'includeStats': False}]}
+
+    rq_res = rqs.post(url='http://localhost:7474/db/data/transaction/commit', headers=headers, data=ujson.dumps(data),
+                      auth=(credential.neo4j_username, credential.neo4j_password))
+    rows = []
+
+    print(rq_res.text)
+
+    jsResp = ujson.loads(rq_res.text)
+
+    for row in jsResp["results"][0]["data"]:
+        rows.append(row['row'])
+
+    df = pandas.DataFrame(rows, columns=jsResp['results'][0]['columns'])
     # convert result to dataFrame
-    df = pandas.DataFrame(result)
+    # df = pandas.DataFrame(result)
 
     # change column type
     df['index'] = df['index'].astype(int)
@@ -85,11 +102,11 @@ def post_combined():
                 # user selected nodes to ignore!
                 df = df[~df['lineage'].str.contains(key)]
             elif in_params_selection[key] == 2:
-                # since the node is included in the cut (match query) remove all child of node selection
                 df = df[~(df['lineage'].str.contains(key) & ~df['id'].str.contains(key))]
 
     # create a pivot_table where columns are samples and rows are features
-    df_pivot = pandas.pivot_table(df,rows=["id", "label", "index", "lineage", "lineageLabel", "start", "end", "order"], cols="s.id", values="agg", fill_value=0).sortlevel("index")
+    df_pivot = pandas.pivot_table(df, rows=["id", "label", "index", "lineage", "lineageLabel", "start", "end", "order"],
+                                  cols="s.id", values="agg", fill_value=0).sortlevel("index")
 
     # convert the pivot matrix into metaviz json format
 
@@ -112,16 +129,10 @@ def post_combined():
     errorStr = ""
     resRowsCols = {"cols": cols, "rows": rows, "globalStartIndex": (min(rows['start']))}
     # res = ujson.dumps({"id":reqId, "error": errorStr, "result": resRowsCols})
-    # return res
-    return jsonify({"id":reqId, "error": errorStr, "result": resRowsCols})
+    res = Response(response=ujson.dumps({"id": reqId, "error": errorStr, "result": resRowsCols}), status=200, mimetype="application/json")
+    return res
+    # return jsonify({"id":reqId, "error": errorStr, "result": resRowsCols})
 
-# if child of parent (node id's), return true
-def parent(child, parent):
-    qryStr = "MATCH (f:Feature {id:'" + child + "'}) RETURN f.lineage as lineage"
-    result = cypher.data(qryStr)
-    if parent in result[0]['lineage']:
-        # print parent, child, result[0]['lineage']
-        return True
 
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0", port=5005)
