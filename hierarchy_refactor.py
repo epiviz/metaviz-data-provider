a098aa963aadc5e3c598c69c9cdef7f26afde592
Jayaram Kancherla <jayaram.kancherla@gmail.com>
2016-08-11 08:10:21 -0400

    use order, selection, selectedLevels, and root node from params

--- hierarchy_refactor.py
+++ hierarchy_refactor.py
@@ -4,8 +4,12 @@ import pandas
 
 def get_data(in_params_selection, in_params_order, in_params_selected_levels, in_params_nodeId, in_params_depth):
 
-    rootNode = "0-0"
-    qryStr = "MATCH (f:Feature {id:'0-0'})-[:PARENT_OF*0..3]->(f2:Feature) " \
+    root_node = in_params_nodeId
+
+    if in_params_nodeId == "":
+        root_node = "0-0"
+
+    qryStr = "MATCH (f:Feature {id:'" + root_node + "'})-[:PARENT_OF*0..3]->(f2:Feature) " \
                 "with collect(f2) + f as nodesFeat unwind nodesFeat as ff " \
                 "return distinct ff.lineage as lineage, ff.start as start, ff.label as label, " \
                 "ff.leafIndex as leafIndex, ff.parentId as parentId, ff.depth as depth, ff.partition as partition, " \
@@ -17,13 +21,33 @@ def get_data(in_params_selection, in_params_order, in_params_selected_levels, in
     rq_res = utils.cypher_call(qryStr)
     df = utils.process_result(rq_res)
 
-    root = df[df['id'].str.contains(rootNode)].iloc[0]
-    other = df[~df['id'].str.contains(rootNode)]
+    # convert columns to int
+    df['index'] = df['index'].astype(int)
+    df['start'] = df['start'].astype(int)
+    df['end'] = df['end'].astype(int)
+    df['order'] = df['order'].astype(int)
+    df['leafIndex'] = df['leafIndex'].astype(int)
+    df['nchildren'] = df['nchildren'].astype(int)
+    df['nleaves'] = df['nleaves'].astype(int)
+    df['depth'] = df['depth'].astype(int)
+
+    # restore current order, selection and levels from input params
+    for key in in_params_order.keys():
+        df.loc[df['id'] == key, 'order'] = in_params_order[key]
+
+    for key in in_params_selection.keys():
+        df.loc[df['id'] == key, 'selectionType'] = in_params_selection[key]
+
+    for key in in_params_selected_levels.keys():
+        df.loc[df['depth'] == key, 'selectionType'] = in_params_selection[key]
+
+    root = df[df['id'].str.contains(root_node)].iloc[0]
+    other = df[~df['id'].str.contains(root_node)]
 
     rootDict = row_to_dict(root)
-    resRows = df_to_tree(rootDict, other)
+    result = df_to_tree(rootDict, other)
 
-    print(ujson.dumps(resRows))
+    print(ujson.dumps(result))
 
 def row_to_dict(row):
     toRet = {}
@@ -56,6 +80,7 @@ def df_to_tree(root, df):
     children = df[df['parentId'].str.contains(root['id'])]
     otherChildren = df[~df['parentId'].str.contains(root['id'])]
     children.sort_values('order')
+    root['size'] = len(children)
 
     for index,row in children.iterrows():
         childDict = row_to_dict(row)
