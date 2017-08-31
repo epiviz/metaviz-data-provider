ae66a7745148b4a49c4783ca8b13ebd775eb39ca
Jayaram Kancherla <jayaram.kancherla@gmail.com>
2017-08-31 11:33:16 -0400

    update missing redirect handler

--- metavizRoute.py
+++ metavizRoute.py
@@ -35,8 +35,8 @@ def add_cors_headers(response):
 
 application.after_request(add_cors_headers)
 
-@application.route('/workspace/', methods = ['POST', 'OPTIONS', 'GET'])
-@application.route('/workspace', methods = ['POST', 'OPTIONS', 'GET'])
+@application.route('/api/workspace/', methods = ['POST', 'OPTIONS', 'GET'])
+@application.route('/api/workspace', methods = ['POST', 'OPTIONS', 'GET'])
 def workspace_api():
     workspaceId = request.args.get('ws')
     queryString = request.args.get('q')
@@ -46,6 +46,32 @@ def workspace_api():
     else :
         res = WorkspaceRequest.get_data(workspaceId, queryString)
         res = Response(response=ujson.dumps({"requestId": reqId, "type": "response", "data": [{"content": res, "id": None, "id_v1": None, "name": "IHMP_ibd_1", "version": "4"}]}), status=200, mimetype="application/json")
+    return res
+
+# Route for POST, OPTIONS, and GET requests
+@application.route('/api/ihmp_redirect/', methods = ['POST', 'OPTIONS', 'GET'])
+@application.route('/api/ihmp_redirect', methods = ['POST', 'OPTIONS', 'GET'])
+def process_redirect():
+    """
+    Send a request to the RedirectRequest class to lookup Metaviz workspace id of iHMP file id
+    Args:
+    Returns:
+     res: Flask redirect containing Metaviz workspace
+    """
+
+    # For OPTIONS request, return an emtpy response
+    if request.method == 'OPTIONS':
+        res = jsonify({})
+        res.headers['Access-Control-Allow-Origin'] = '*'
+        res.headers['Access-Control-Allow-Headers'] = '*'
+        return res
+
+    in_params_id = request.values['fid']
+
+    if(in_params_id != None):
+        redirect_location = RedirectRequest.get_data(in_params_id)
+
+    res = redirect(redirect_location, code=302)
     return res
 
 # Route for POST, OPTIONS, and GET requests
